70c88876dd795f6947563b93be68fd28d0a18bd4
FizzyApps <72231376+Fizzyapps@users.noreply.github.com>
2024-10-14 14:51:29 -0400

    Update lineage_rtwo.mk

--- lineage_rtwo.mk
+++ lineage_rtwo.mk
@@ -15,47 +15,6 @@ $(call inherit-product, device/motorola/rtwo/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 
-# disable/enable blur support, default is false
-TARGET_ENABLE_BLUR := true
-
-# whether to ship aperture camera, default is false
-PRODUCT_NO_CAMERA := true
-
-# Wether to ship lawnchair launcher
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    RISING_CHIPSET="SnapDragon 8gen2" \
-    RISING_MAINTAINER="FizzyApps"
-
-
-
-RISING_PACKAGE_TYPE := Gapps
-
-WITH_GMS := true
-
-TARGET_CORE_GMS := true
-
-
-# extra add-ons for core GMS builds
-# List of add-ons
-# PRODUCT_PACKAGES += \
-#    Photos \
-#    MarkupGoogle \
-#    LatinIMEGooglePrebuilt \
-#    AiWallpapers \
-#    WallpaperEmojiPrebuilt \
-#    PrebuiltDeskClockGoogle \
-#    CalculatorGooglePrebuilt \
-#    CalendarGooglePrebuilt \
-#    Velvet
-TARGET_CORE_GMS_EXTRAS := true
-
-
-# Wether to ship pixel launcher and set it as default launcher
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
-
-
 
 PRODUCT_NAME := lineage_rtwo
 PRODUCT_DEVICE := rtwo
